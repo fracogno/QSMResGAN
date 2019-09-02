3ec4f975b0f80747c16d3a0c81c97bf488dae92f
fracogno <francesco.cognolato@hotmail.com>
2019-09-02 21:33:04 +1000

    ResUNET

--- src/ResUNET.py
+++ src/ResUNET.py
@@ -10,49 +10,43 @@ def downsample(inputs):
 
 
 def residualBlock(inputs, numFilters, kernelSize, isTraining, encoding=True):
-	print(inputs)
-	
 	'''
 		First part
 	'''
-	# Convolve input
 	conv1 = tf.layers.conv3d(inputs, numFilters, kernelSize, 1, 'SAME', use_bias=False, kernel_initializer='he_normal')
-	#bn1 = tf.layers.batch_normalization(conv1, training=isTraining)
+	#tf.layers.batch_normalization(conv1, training=isTraining)
 	relu1 = tf.nn.leaky_relu(conv1)
 	print(relu1)
 
-	# Convolve result of conv input
 	conv2 = tf.layers.conv3d(relu1, numFilters, kernelSize, 1, 'SAME', use_bias=False, kernel_initializer='he_normal')
 	print(conv2)
 
-	# Resize convolved input
 	resizedConvInput = downsample(conv2) if encoding else upsample(conv2)
 	print(resizedConvInput)
 
 	'''
 		Second part
 	'''
-	# Resize original input
 	resizedInput = downsample(inputs) if encoding else upsample(inputs)
 	print(resizedInput)
 
-	# Convolve resized input
 	convInput = tf.layers.conv3d(resizedInput, numFilters, kernelSize, 1, 'SAME', use_bias=False, kernel_initializer='he_normal')
-	#bn2 = tf.layers.batch_normalization(conv2, training=isTraining)
+	#tf.layers.batch_normalization(conv2, training=isTraining)
 	print(convInput)
 
-	# Skip connection
+	'''
+		Skip connection
+	'''
 	output = resizedConvInput + convInput
 	print(output)
 
 	output = tf.nn.leaky_relu(output)
-	print(output)
-	print()
+	print(str(output) + "\n")
 
 	return output
 
 
-def getGenerator(X, reuse=False):
+def getGenerator(X, reuse=False, kernelSize=4):
 	filters = [64, 128, 256, 512, 512, 512]
 
 	with tf.variable_scope("generator", reuse=reuse):
@@ -63,22 +57,21 @@ def getGenerator(X, reuse=False):
 		skips = []
 		skips.append(output)
 		for numFilters in filters:
-			output = residualBlock(output, numFilters, 4, None)
+			output = residualBlock(output, numFilters, kernelSize, None)
 			skips.append(output)
 
 		# Decoder
-		skips = reversed(skips[:-1])
-		for numFilters, skip in zip(reversed(filters), skips):
-			output = residualBlock(output, numFilters, 4, None, False)
+		for numFilters, skip in zip(reversed(filters), reversed(skips[:-1])):
+			output = residualBlock(output, numFilters, kernelSize, None, False)
 			output = tf.concat([output, skip], axis=4)
 
-		last = tf.layers.conv3d(output, 1, 4, 1, 'SAME', use_bias=False, kernel_initializer='he_normal')
+		last = tf.layers.conv3d(output, 1, kernelSize, 1, 'SAME', use_bias=False, kernel_initializer='he_normal')
 		print(last)
 
 		return last
 
 
-def getDiscriminator(X, Y, reuse=False):
+def getDiscriminator(X, Y, reuse=False, kernelSize=4):
 	filters = [64, 128]
 
 	with tf.variable_scope('discriminator', reuse=reuse):
@@ -88,12 +81,12 @@ def getDiscriminator(X, Y, reuse=False):
 		for numFilters in filters:
 			output = residualBlock(output, numFilters, 4, None)
 
-		conv = tf.layers.conv3d(output, 256, 4, strides=1, use_bias=False, kernel_initializer='he_normal')
+		conv = tf.layers.conv3d(output, 256, kernelSize, strides=1, use_bias=False, kernel_initializer='he_normal')
 		#bn = tf.contrib.layers.batch_norm(conv, decay=0.9, is_training=True, updates_collections=None, epsilon=1e-5, scale=True)
 		lrelu = tf.nn.leaky_relu(conv)
 		print(lrelu)
 
-		last = tf.layers.conv3d(lrelu, 1, 4, 1, 'SAME', use_bias=False, kernel_initializer='he_normal')
+		last = tf.layers.conv3d(lrelu, 1, kernelSize, 1, 'SAME', use_bias=False, kernel_initializer='he_normal')
 		print(last)
 
 		return last
