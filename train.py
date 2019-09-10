b951df4fc3eee2569af5a0de260e64aa59f63ee5
fracogno <francesco.cognolato@hotmail.com>
2019-09-10 14:06:54 +1000

    Train GAN validation

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 import numpy as np
-import datetime
 import nibabel as nib
-import src.LinkNet as network, src.utilities as util
+from datetime import datetime
+import src.ResUNet as network, src.utilities as util, src.loss as loss
 
+# Set GPU ??
 tf.logging.set_verbosity(tf.logging.ERROR)
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
@@ -14,28 +14,22 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0"
 base_path = "/scratch/cai/deepQSMGAN/"
 data_path = "data/shapes_shape64_ex100_2019_08_30"
 
-'''
-    Parameters for training
-'''
-epochs = 2500
-batch_size = 1
-lr = 0.0001
+base_path ="/home/francesco/UQ/deepQSMGAN/"
+data_path = "data/shapes_shape64_ex100_2019_08_20"
+checkpointsPath = base_path + "ckp_" + datetime.now().strftime("%Y%m%d_%H%M") + "_" + data_path.split("/")[-1]
+
+# Parameters
+epochs = 3000
+batchSize = 1
+lr = 1e-4
 beta1 = 0.5
-l1_weight = 100.0
+L1_Weight = 100.0
 labelSmoothing = 0.9
 
-# Create checkpoints path
-tf.reset_default_graph()
-now = datetime.datetime.now()
-checkpointName = "ckp_" + str(now.year) + str(now.month) + str(now.day) + "_" + str(now.hour) + str(now.minute) + "_" + data_path.split("/")[-1]
-
-'''
-    Import data
-'''
-# Training
+# Training data
 input_shape = (64, 64, 64, 1)
 train_data_filename = util.generate_file_list(file_path=base_path + data_path + "/train/", p_shape=input_shape)
-train_input_fn = util.data_input_fn(train_data_filename, p_shape=input_shape, batch=batch_size, nepochs=epochs, shuffle=True)
+train_input_fn = util.data_input_fn(train_data_filename, p_shape=input_shape, batch=batchSize, nepochs=epochs, shuffle=True)
 X_tensor, Y_tensor, _ = train_input_fn()
 
 # Validation
@@ -68,56 +62,29 @@ print("X val padded to 2^n multiple: " + str(X_val.shape))
 print("Y val: " + str(Y_val.shape))
 print("Mask shape: " + str(mask.shape))
 
-'''
-    Define graphs for the networks
-'''
+# Networks
 Y_generated = network.getGenerator(X_tensor)    
 Y_val_generated = network.getGenerator(X_val_tensor, True)
 
 D_logits_real = network.getDiscriminator(X_tensor, Y_tensor)
 D_logits_fake = network.getDiscriminator(X_tensor, Y_generated, True)
 
-'''
-    Loss functions
-'''
-def discriminatorLoss(dis_real, dis_fake, smoothing=1.0):
-    dis_real_ce = tf.nn.sigmoid_cross_entropy_with_logits(logits=dis_real, labels=tf.ones_like(dis_real) * smoothing)
-    dis_fake_ce = tf.nn.sigmoid_cross_entropy_with_logits(logits=dis_fake, labels=tf.zeros_like(dis_fake))
+# Losses and optimizer
+D_loss = loss.discriminatorLoss(D_logits_real, D_logits_fake, labelSmoothing)
+G_loss, G_gan, G_L1 = loss.generatorLoss(D_logits_fake, Y_generated, Y_tensor, L1_Weight)
+optimizer = loss.getOptimizer(lr, beta1, D_loss, G_loss)
 
-    dis_loss_real = tf.reduce_mean(dis_real_ce)
-    dis_loss_fake = tf.reduce_mean(dis_fake_ce)
-    dis_loss = tf.reduce_mean(dis_real_ce + dis_fake_ce)
+# Print variables
+for i in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator'):
+    print(i)
+for i in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator'):
+    print(i)
 
-    return dis_loss
-
-def generatorLoss(dis_fake, G_output, target, weight):
-    gen_ce = tf.nn.sigmoid_cross_entropy_with_logits(logits=dis_fake, labels=tf.ones_like(dis_fake))
-
-    gen_loss_gan = tf.reduce_mean(gen_ce)
-    gen_loss_l1 = tf.reduce_mean(tf.abs(target - G_output)) 
-    gen_loss = gen_loss_gan + (gen_loss_l1 * weight)
-
-    return gen_loss, gen_loss_gan, gen_loss_l1
-
-D_loss = discriminatorLoss(D_logits_real, D_logits_fake, labelSmoothing)
-G_loss, G_gan, G_L1 = generatorLoss(D_logits_fake, Y_generated, Y_tensor, l1_weight)
-
-'''
-    Optimizers for weights
-'''
-D_optimizer = tf.train.AdamOptimizer(lr, beta1).minimize(D_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator'))
-with tf.control_dependencies([D_optimizer]):
-    G_optimizer = tf.train.AdamOptimizer(lr, beta1).minimize(G_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator'))
-train_op = G_optimizer
-
-'''
-    SUMMARIES - Create a list of summaries
-'''
-# Training
+# Tensorboard
 train_summaries = [tf.summary.scalar('D_loss', D_loss), tf.summary.scalar('G_loss', G_gan), tf.summary.scalar('L1_loss', G_L1), \
-					tf.summary.image('input', X_tensor[:, :, :, int(input_shape[2]/2)], max_outputs=1), \
-					tf.summary.image('output', Y_generated[:, :, :, int(input_shape[2]/2)], max_outputs=1), \
-					tf.summary.image('ground_truth', Y_tensor[:, :, :, int(input_shape[2]/2)], max_outputs=1)]
+                    tf.summary.image('input', X_tensor[:, :, :, int(input_shape[2]/2)], max_outputs=1), \
+                    tf.summary.image('output', Y_generated[:, :, :, int(input_shape[2]/2)], max_outputs=1), \
+                    tf.summary.image('ground_truth', Y_tensor[:, :, :, int(input_shape[2]/2)], max_outputs=1)]
 
 train_merged_summaries = tf.summary.merge(train_summaries)
 
@@ -132,13 +99,10 @@ calcStreakTensor = tf.placeholder(tf.float32, shape=())
 deviationFromCalcMomentTensor = tf.placeholder(tf.float32, shape=())
 
 val_summaries = [tf.summary.scalar('rmse', rmseTensor), tf.summary.scalar('ddrmse', ddrmseTensor), tf.summary.scalar('ddrmseTissue', ddrmseTissueTensor), \
-				 tf.summary.scalar('ddrmseBlood', ddrmseBloodTensor), tf.summary.scalar('ddrmseDGM', ddrmseDGMTensor), \
-				 tf.summary.scalar('deviationFromLinearSlope', deviationFromLinearSlopeTensor), tf.summary.scalar('calcStreak', calcStreakTensor), \
-				 tf.summary.scalar('deviationFromCalcMoment', deviationFromCalcMomentTensor)]
+                 tf.summary.scalar('ddrmseBlood', ddrmseBloodTensor), tf.summary.scalar('ddrmseDGM', ddrmseDGMTensor), \
+                 tf.summary.scalar('deviationFromLinearSlope', deviationFromLinearSlopeTensor), tf.summary.scalar('calcStreak', calcStreakTensor), \
+                 tf.summary.scalar('deviationFromCalcMoment', deviationFromCalcMomentTensor)]
 val_merged_summaries = tf.summary.merge(val_summaries)
-
-# VISUALIZE => tensorboard --logdir=.
-summaries_dir = base_path + checkpointName
 
 # Array containing best metrics values
 bestValMetrics = [1e6] * len(util.getMetrics(Y_val, Y_val, mask, finalSegment))
@@ -151,21 +115,18 @@ with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
 
     # op to write logs to Tensorboard
-    train_summary_writer = tf.summary.FileWriter(summaries_dir + '/train', graph=tf.get_default_graph())
-    val_summary_writer = tf.summary.FileWriter(summaries_dir + '/val')
+    train_summary_writer = tf.summary.FileWriter(checkpointsPath + '/train', graph=tf.get_default_graph())
+    val_summary_writer = tf.summary.FileWriter(checkpointsPath + '/val')
 
-    global_step = 0
+    globalStep = 0
     while True:
         try:
             # Training step
-            if global_step % 100 == 0:
-                _, summary = sess.run([train_op, train_merged_summaries])
-                train_summary_writer.add_summary(summary, global_step)
-            else:
-                sess.run(train_op)
-            
+            _, summary = sess.run([optimizer, train_merged_summaries])
+            train_summary_writer.add_summary(summary, globalStep)
+
             # Check validation accuracy
-            if global_step % 250 == 0:
+            if globalStep % 250 == 0:
                 predicted = sess.run(Y_val_generated, feed_dict={ X_val_tensor : [np.expand_dims(X_val, axis=-1)] })
                 
                 #Remove paddings and if it was not even shape
@@ -176,16 +137,12 @@ with tf.Session(config=config) as sess:
 
                 # Calculate metrics over validation and save it
                 metrics = util.getMetrics(Y_val, predicted, mask, finalSegment)
-                summary = sess.run(val_merged_summaries, feed_dict={ rmseTensor: metrics[0],
-                													 ddrmseTensor: metrics[1],
-                													 ddrmseTissueTensor: metrics[2],
-                													 ddrmseBloodTensor: metrics[3],
-                													 ddrmseDGMTensor: metrics[4],
-                													 deviationFromLinearSlopeTensor: metrics[5],
-                													 calcStreakTensor: metrics[6],
-                													 deviationFromCalcMomentTensor: metrics[7] })
-                val_summary_writer.add_summary(summary, global_step)
-                saver.save(sess, summaries_dir + "/model-save-always")
+                summary = sess.run(val_merged_summaries, feed_dict={ rmseTensor: metrics[0], ddrmseTensor: metrics[1],
+                                                                     ddrmseTissueTensor: metrics[2], ddrmseBloodTensor: metrics[3],
+                                                                     ddrmseDGMTensor: metrics[4], deviationFromLinearSlopeTensor: metrics[5],
+                                                                     calcStreakTensor: metrics[6], deviationFromCalcMomentTensor: metrics[7] })
+                val_summary_writer.add_summary(summary, globalStep)
+                saver.save(sess, checkpointsPath + "/model-save-always")
 
                 # Iterate over metrics
                 print(metrics)
@@ -194,7 +151,7 @@ with tf.Session(config=config) as sess:
                     # If better value of metric, save it
                     if metrics[numMetric] < bestValMetrics[numMetric]:
                         bestValMetrics[numMetric] = metrics[numMetric]
-                        saver.save(sess, summaries_dir + "/model-metric" + str(numMetric))
-            global_step += 1 
+                        saver.save(sess, checkpointsPath + "/model-metric" + str(numMetric))
+            globalStep += 1 
         except tf.errors.OutOfRangeError:
             break
