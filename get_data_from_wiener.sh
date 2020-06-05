b6b2862976350a6847ee83cd8785f1c5207b9bf9
Francesco <francesco.cognolato@gmail.com>
2020-06-05 16:01:32 +1000

    Get data from wiener + gitignore

--- get_data_from_wiener.sh
+++ get_data_from_wiener.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync --exclude 'model*' -avz -e ssh uqfcogno@wiener.hpc.net.uq.edu.au:/scratch/cai/QSMResGAN/ckp* .
+#rsync --exclude 'model*' -avz -e ssh uqfcogno@wiener.hpc.net.uq.edu.au:/scratch/cai/QSMResGAN/ckp* .
+rsync --exclude 'model*' -avz -e ssh uqfcogno@wiener.hpc.net.uq.edu.au:/scratch/cai/QSMResGAN/ckp* . --exclude '*.data*'
