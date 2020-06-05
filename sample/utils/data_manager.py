abacf680450f7566e00004328987a67d577359b8
Francesco <francesco.cognolato@gmail.com>
2020-06-05 14:35:41 +1000

    Padding fix

--- sample/utils/data_manager.py
+++ sample/utils/data_manager.py
@@ -16,13 +16,15 @@ def get_QSM_datasets(base_path, training_data_folder, qsm_challenge_2017_folder,
         datasets["val"] = records_manager.load_dataset(base_path + training_data_folder + "/val/", shape, batch_size)
 
     # QSM challenges data
-    datasets["qsm_2017"] = get_QSM_challenge_2017_data(base_path + qsm_challenge_2017_folder, (192, 192, 192), normalize=False)
-    datasets["qsm_2019"] = get_QSM_challenge_2019_data(base_path + qsm_challenge_2019_folder, (192, 256, 256), normalize=True)
+    datasets["qsm_2017"] = get_QSM_challenge_2017_data(base_path + qsm_challenge_2017_folder, (256, 256, 256), normalize=False)
+    datasets["qsm_2019"] = get_QSM_challenge_2019_data(base_path + qsm_challenge_2019_folder, (256, 256, 256), normalize=True)
 
     return datasets
 
 
-def get_QSM_challenge_2017_data(base_path, normalize):
+def get_QSM_challenge_2017_data(base_path, pad_size, normalize):
+    initial_shape, pad_axis = None, None
+
     x = misc.load_nii(base_path + "/data/phs_tissue.nii.gz")
     if normalize:
         TEin_s = 8 / 1000
@@ -30,15 +32,21 @@ def get_QSM_challenge_2017_data(base_path, normalize):
         centre_freq = 297190802
         x = frequency_rad / (2 * np.pi * TEin_s * centre_freq) * 1e6
 
-    y = misc.load_nii(base_path + "/data/chi_33.nii.gz")
-    mask = misc.load_nii(base_path + "/data/msk.nii.gz")
+    x = np.float32(np.expand_dims(np.array([x]), -1))
+    y = np.float32(np.expand_dims(np.array([misc.load_nii(base_path + "/data/chi_33.nii.gz")]), -1))
+    mask = np.float32(np.expand_dims(np.array([misc.load_nii(base_path + "/data/msk.nii.gz")]), -1))
 
-    x, y, mask = np.array([x]), np.array([y]), np.array([mask])
+    if not pad_size is None:
+        x, initial_shape, pad_axis = misc.add_padding(x, pad_size)
+        y, _, _ = misc.add_padding(y, pad_size)
+        mask, _, _ = misc.add_padding(mask, pad_size)
+
     assert (x.shape == y.shape and x.shape == mask.shape)
-
-    return [{"x": np.float32(np.expand_dims(x, -1)),
-             "y": np.float32(np.expand_dims(y, -1)),
-             "mask": np.float32(np.expand_dims(mask, -1)),
+    return [{"x": x,
+             "y": y,
+             "mask": mask,
+             "initial_shape": initial_shape,
+             "pad_axis": pad_axis,
              "name": "vn"}]
 
 
@@ -56,13 +64,13 @@ def get_QSM_challenge_2019_data(base_path, pad_size, normalize):
         x = np.float32(np.expand_dims(np.array([x]), -1))
         y = np.float32(np.expand_dims(np.array([misc.load_nii(path + "/GT/Chi.nii.gz")]), -1))
         mask = np.float32(np.expand_dims(np.array([misc.load_nii(path + "/MaskBrainExtracted.nii.gz")]), -1))
-        assert (x.shape == y.shape and x.shape == mask.shape)
 
         if not pad_size is None:
             x, initial_shape, pad_axis = misc.add_padding(x, pad_size)
             y, _, _ = misc.add_padding(y, pad_size)
             mask, _, _ = misc.add_padding(mask, pad_size)
 
+        assert (x.shape == y.shape and x.shape == mask.shape)
         qsm_2019.append({"x": x,
                          "y": y,
                          "mask": mask,
