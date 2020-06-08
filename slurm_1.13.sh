b30828376cf956f7edbb71fc21fc6eeffb9d4424
fracogno <francesco.cognolato@hotmail.com>
2020-06-08 21:58:59 +1000

    Slurm

--- slurm_1.13.sh
+++ slurm_1.13.sh
@@ -15,5 +15,5 @@ module load openmpi3
 module load anaconda/3.6
 source activate /opt/ohpc/pub/apps/tensorflow_1.13
 
-srun -n 1 python3 src/train_NEW.py
-# srun -n 1 python3 src/predict.py
+#srun -n 1 python3 src/train_NEW.py
+srun -n 1 python3 src/predict.py
